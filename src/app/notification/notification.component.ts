ffcd4e56ab29d829ea437d554e4f2b7191a3db05
Ravi Patel <ravipatel84184@gmail.com>
2023-08-05 11:44:53 +0530

    init comment

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: '<div class="alert alert-success"><p>This website uses cookies to provide better user experience.</p></div>',
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent {
+
+}
